504fdbf2349acf9dca5f8ffbd6da6fc841064bff
Natasha <nat4shail@yandex.ru>
2023-08-13 15:25:48 +0300

    hx

--- mail.php
+++ mail.php
@@ -6,12 +6,16 @@ $method = $_SERVER['REQUEST_METHOD'];
 $c = true;
 if ( $method === 'POST' ) {
 
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
+  $name = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+	$name = trim($_POST["username"]);
+	$email  = trim($_POST["email"]);
+	$password = trim($_POST["password"]);
 
 	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		if ( $value != "" && $key != "" && $key != "email" && $key != "form_subject" ) {
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
@@ -24,7 +28,7 @@ if ( $method === 'POST' ) {
 
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
-	$form_subject = trim($_GET["form_subject"]);
+	$password = trim($_GET["password"]);
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
@@ -46,7 +50,9 @@ function adopt($text) {
 
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
+'From: '.adopt($name).' <'.$email.'>' . PHP_EOL .
+'Reply-To: '.$email.'' . PHP_EOL;
 
-mail($admin_email, adopt($form_subject), $message, $headers );
+mail($email, adopt($password), $message, $headers );
+
+  
