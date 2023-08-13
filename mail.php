9bd2cf86b9b2e9ce73e4612bf6d7bd870a062d4c
Natasha <nat4shail@yandex.ru>
2023-08-13 15:27:12 +0300

    j

--- mail.php
+++ mail.php
@@ -1,58 +1,26 @@
-<?php
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-//Script Foreach
-$c = true;
-if ( $method === 'POST' ) {
-
-  $name = $_POST['username'];
+<?php 
+$name = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-	$name = trim($_POST["username"]);
-	$email  = trim($_POST["email"]);
-	$password = trim($_POST["password"]);
 
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "" && $key != "email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
-} else if ( $method === 'GET' ) {
-
-	$project_name = trim($_GET["project_name"]);
-	$admin_email  = trim($_GET["admin_email"]);
-	$password = trim($_GET["password"]);
-
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+if (trim($name) == "") 
+  echo "Вы не ввели имя пользователя";
+else if(strlen(trim($name)) <= 1)
+  echo "Такого имени не существует";
+else if(trim($email) == "" || trim($password) == "" || "" || trim($_POST["message"]) == "")
+  echo "Введите все двнные";
+else {
+  $_POST['password'] = md5($password);
+  echo "<h1>Все данные</h1>";
+  foreach ($_POST as $key => $value) 
+    echo "<p>$value</p>";
 }
 
-$message = "<table style='width: 100%;'>$message</table>";
 
-function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
-}
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($name).' <'.$email.'>' . PHP_EOL .
-'Reply-To: '.$email.'' . PHP_EOL;
-
-mail($email, adopt($password), $message, $headers );
-
-  
+if (mail("nat4shail@yandex.ru", "Заказ с сайта", "ФИО:".$name.". E-mail: ".$email))
+ {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+} 
